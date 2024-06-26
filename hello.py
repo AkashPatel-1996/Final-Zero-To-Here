52ead37906fd227b066dfd588bf41a6482df8223
AkashPatel-1996 <84029971+AkashPatel-1996@users.noreply.github.com>
2024-06-26 12:35:42 +0000

    daya

--- hello.py
+++ hello.py
@@ -1,12 +1,5 @@
+def add(x,y):
+    return x+y
 
-my_list = ["apple", "cherry", "lemon"]
-my_dict = {"drink": "coffee", "milk": "whole"}
 
-for item in my_list:
-    print(f"My favorite thing to eat is: {item}")
-
-for _, value in my_dict.items():
-    print(f"My favorite thing to drink is: {value}")
-
-# my_dict=my_dict
-# my_list2=
+print(add(2,32))
